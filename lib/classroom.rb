702211e7c6604d315d004f7dd2d7fbb66dcfbd6e
Sawaez Amin <sawaezamin@gmail.com>
2021-05-12 15:20:57 -0600

    Initial Commit

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,17 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize (subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students(student)
+    @students = yell_at_students.each.upcase
+  end
 end
