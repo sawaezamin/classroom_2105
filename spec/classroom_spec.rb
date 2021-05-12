6d71ad35934f58ba881cfc75cceea26ae2ee8255
Sawaez Amin <sawaezamin@gmail.com>
2021-05-12 15:50:13 -0600

    Update final changes

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -44,5 +44,57 @@ describe Classroom do
 
       expect(classroom.yell_at_students).to eq ['MIKE', 'MEGAN', 'BOB']
     end
+
+# Iteration 3 attempt
+    it 'returns a boolean for class limit' do
+      classroom = Classroom.new('History', 4)
+
+      classroom.over_capacity?
+
+      expect(classroom.over_capacity).to eq ('false')
+    end
+
+    it 'adds new students to existing list'
+      classroom = Classroom.new('History', 4)
+      classroom.add_student('Mike')
+      classroom.add_student('Megan')
+      classroom.add_student('Bob')
+      classroom.add_student('Eve')
+      classroom.add_student('Alice')
+
+      expect(classroom.students).to eq ['Mike', 'Megan', 'Bob', 'Eve', 'Alice']
   end
+
+  it 'gives new boolean for class capacity' do
+    classroom = Classroom.new('History', 4)
+
+    classroom.over_capacity?
+
+    expect(classroom.over_capacity).to eq ('true')
+  end
+
+  #Iteration 4 attempt
+  it 'removes a student from class' do
+    classroom = Classroom.new('History', 4)
+    classroom.add_student('Mike')
+    classroom.add_student('Megan')
+    classroom.add_student('Bob')
+    classroom.add_student('James')
+    classroom.add_student('Cat')
+    classroom.add_student('Alice')
+
+    classroom.kick_out
+
+    expect(classroom.over_capacity).to eq ('true')
+  end
+
+  it 'gives new list of students in class'
+  classroom = Classroom.new('History', 4)
+  classroom.add_student('Bob')
+  classroom.add_student('James')
+  classroom.add_student('Cat')
+  classroom.add_student('Alice')
+
+  expect(classroom.over_capacity).to eq('false')
+  expect(classroom.students).to eq (['Bob', 'James', 'Cat', 'Alice'])
 end
